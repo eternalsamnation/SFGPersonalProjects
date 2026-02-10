64b7f066988efe0daab94385df9675b34111ab20
eternalsamnation <sfindlengolden@outlook.com>
2026-02-09 22:36:21 -0500

    README + formatting

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # SFGPersonalProjects
 Version controlling some summary projects I've been working on!
+
+## Modules
+To run Python modules, configure desired functions in `main.py` and run from the terminal accordingly
+
+## Dashboards
+To run dashboards, enter `streamlit run dashboards/{DASHBOARD_NAME}.py` in the terminal
+
+## Utils
+Currently just contains config constants across functions
--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 from utils.config import *
-from modules.chatgpt_summary.chatgpt_stats import run_chatgpt_stats
-from modules.format_converters.heic_to_jpg import *
-from modules.format_converters.mov_to_mp4 import convert_mov_to_mp4
 from modules.song_hub.spotify_library import fetch_spotify_library
 from modules.song_hub.yt_to_mp3 import download_single_video, download_video_list, download_playlist
 from modules.song_hub.spotify_downloader_ytdlp import ytdlp_song_downloader
 from modules.song_hub.spotify_downloader_selenium import selenium_song_downloader, selenium_song_downloader_jr
+from modules.chatgpt_summary.chatgpt_stats import run_chatgpt_stats
+from modules.instagram_summary.instagram_follower_parser import extract_insta_data
+from modules.format_converters.heic_to_jpg import *
+from modules.format_converters.mov_to_mp4 import convert_mov_to_mp4
 
-# run_chatgpt_stats()
-# convert_images(SINGLE_IMAGE_FILE_PATH)
-# convert_mov_to_mp4(VIDEO_FOLDER_PATH,VIDEO_OUTPUT_PATH)
+# Song Hub
 # fetch_spotify_library()
-# download_single_video(VIDEO_URL, VIDEO_SAVE_PATH)
-# download_video_list(VIDEO_LIST, VIDEO_SAVE_PATH)
-# download_playlist(PLAYLIST_URL, PLAYLIST_SAVE_PATH)
 # ytdlp_song_downloader()
 # selenium_song_downloader()
 # selenium_song_downloader_jr()
+
+# Summaries
+# run_chatgpt_stats()
+# extract_insta_data()
+
+# Format Converters
+# convert_images(SINGLE_IMAGE_FILE_PATH)
+# convert_mov_to_mp4(VIDEO_FOLDER_PATH,VIDEO_OUTPUT_PATH)
+# download_single_video(VIDEO_URL, VIDEO_SAVE_PATH)
+# download_video_list(VIDEO_LIST, VIDEO_SAVE_PATH)
+# download_playlist(PLAYLIST_URL, PLAYLIST_SAVE_PATH)
